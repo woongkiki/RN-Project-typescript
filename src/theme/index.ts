cd83c8fcc8543ff006c2066171ad10208fbbbcf0
woongkiki <kwk4348@nate.com>
2026-04-02 17:44:16 +0900

    commit 260402 src theme

---
+++ src/theme/index.ts
@@ -0,0 +1,12 @@
+import { BrandTheme } from '../types';
+
+export const defaultTheme: BrandTheme = {
+  colors: {
+    primary: '#007AFF',
+    secondary: '#5856D6',
+    background: '#FFFFFF',
+    text: '#000000',
+    tabBar: '#F8F8F8',
+  },
+  logo: '',
+};
